5b469756723d08420b45af14e0b0ba09268b8d3d
Nathan Dockens <ndockens92@gmail.com>
2023-10-18 16:02:12 -0500

    Refactor

--- src/App/World/Environment.ts
+++ src/App/World/Environment.ts
@@ -1,13 +1,9 @@
 import {
     AmbientLight,
-    BoxGeometry,
     MathUtils,
-    Mesh,
-    MeshStandardMaterial,
-    PlaneGeometry,
     Scene,
 } from 'three'
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import { GLTF, GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 
 import Global from '../Utilities/Global'
 
@@ -17,38 +13,20 @@ export default class Environment {
 
     constructor() {
         this.scene = Global.scene
-
         this.gltfLoader = new GLTFLoader()
-        this.gltfLoader.load('/models/sci_fi_room.glb', (loadedAsset) => {
-            loadedAsset.scene.scale.setScalar(0.1)
-            loadedAsset.scene.rotateY(MathUtils.degToRad(45))
-            this.scene.add(loadedAsset.scene)
-        })
-        
-        // this.addFloor()
-        // this.addCenterpiece()
+
+        this.loadAssets()
         this.addLights()
     }
 
-    private addFloor() {
-        const floorGeometry = new PlaneGeometry(20, 20)
-        const floorMaterial = new MeshStandardMaterial({ color: 'grey' })
-        const floor = new Mesh(floorGeometry, floorMaterial)
-        floor.rotateX(MathUtils.degToRad(-90))
-
-        this.scene.add(floor)
+    private async loadAssets(): Promise<void> {
+        const loadedAsset: GLTF = await this.gltfLoader.loadAsync('/models/sci_fi_room.glb')
+        loadedAsset.scene.scale.setScalar(0.1)
+        loadedAsset.scene.rotateY(MathUtils.degToRad(45))
+        this.scene.add(loadedAsset.scene)
     }
 
-    private addCenterpiece() {
-        const centerpieceGeometry = new BoxGeometry(2, 2, 2)
-        const centerpieceMaterial = new MeshStandardMaterial({ color: 'darkgrey' })
-        const centerpiece = new Mesh(centerpieceGeometry, centerpieceMaterial)
-        centerpiece.position.set(0, 1, 0)
-        
-        this.scene.add(centerpiece)
-    }
-
-    private addLights() {
+    private addLights(): void {
         const ambientLight = new AmbientLight(0xffffff, 0.5)
         this.scene.add(ambientLight)
     }
