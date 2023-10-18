f94f1dd940f54ccbf91644901b37ef3a70a725e1
Nathan Dockens <ndockens92@gmail.com>
2023-10-18 16:11:57 -0500

    Refactor

--- src/App/World/Environment.ts
+++ src/App/World/Environment.ts
@@ -8,6 +8,8 @@ import { GLTF, GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 import Global from '../Utilities/Global'
 
 export default class Environment {
+    private readonly pathToRoomModel: string = '/models/sci_fi_room.glb'
+    
     private gltfLoader: GLTFLoader
     private scene: Scene
 
@@ -15,19 +17,19 @@ export default class Environment {
         this.scene = Global.scene
         this.gltfLoader = new GLTFLoader()
 
-        this.loadAssets()
+        this.addRoom()
         this.addLights()
     }
 
-    private async loadAssets(): Promise<void> {
-        const loadedAsset: GLTF = await this.gltfLoader.loadAsync('/models/sci_fi_room.glb')
+    private async addRoom(): Promise<void> {
+        const loadedAsset: GLTF = await this.gltfLoader.loadAsync(this.pathToRoomModel)
         loadedAsset.scene.scale.setScalar(0.1)
         loadedAsset.scene.rotateY(MathUtils.degToRad(45))
         this.scene.add(loadedAsset.scene)
     }
 
     private addLights(): void {
-        const ambientLight = new AmbientLight(0xffffff, 0.5)
+        const ambientLight = new AmbientLight(0xffffff, 0.7)
         this.scene.add(ambientLight)
     }
 }
