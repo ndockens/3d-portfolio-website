a148af5879720f37cb99b64c882b9bd876bb8a92
Nathan Dockens <ndockens92@gmail.com>
2023-10-20 12:12:40 -0500

    Minor tweaks to camera controls

--- src/App/Controls.ts
+++ src/App/Controls.ts
@@ -2,12 +2,12 @@ import { Camera, MathUtils } from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 export default class Controls {
-    private readonly minDistance: number = 30
-    private readonly maxDistance: number = 250
-    private readonly minPolarAngleDegrees: number = 0
+    private readonly minDistance: number = 80
+    private readonly maxDistance: number = 200
+    private readonly minPolarAngleDegrees: number = 30
     private readonly maxPolarAngleDegrees: number = 90
-    private readonly minAzimuthAngleDegrees: number = -90
-    private readonly maxAzimuthAngleDegrees: number = 90
+    private readonly minAzimuthAngleDegrees: number = -45
+    private readonly maxAzimuthAngleDegrees: number = 45
 
     private instance: OrbitControls
 
