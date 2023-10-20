4ad0027eed427eb9acd78927c25133e46e0fe2a0
Nathan Dockens <ndockens92@gmail.com>
2023-10-20 14:00:44 -0500

    Fix issue with text visibility

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import App from './App/App'
 
 new App()
 
-const message = document.getElementsByClassName('initial-message')[0] as HTMLElement
+const message = document.getElementsByClassName('message')[0] as HTMLElement
 
 if (message !== null) {
     setTimeout(() => {
         message.classList.add('fadeIn')
+        message.classList.remove('fadeOut')
         setTimeout(() => {
             message.classList.add('fadeOut')
+            message.classList.remove('fadeIn')
             setTimeout(() => message.style.display = 'none', 5000)
         }, 7000)
     }, 1000)
