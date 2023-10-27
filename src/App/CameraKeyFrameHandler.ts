7f77c3cb6632f3e1f08220f8c2a0d187424914f0
Nathan Dockens <ndockens92@gmail.com>
2023-10-27 18:12:16 -0500

    Refactor

--- src/App/CameraKeyFrameHandler.ts
+++ src/App/CameraKeyFrameHandler.ts
@@ -3,21 +3,29 @@ import { Vector3 } from 'three'
 import Camera from './Camera'
 import Global from './Utilities/Global'
 
+interface CameraKeyFrame {
+    id: number
+    position: Vector3
+    target: Vector3
+    triggerType: string
+    triggerValue: string
+}
+
 export default class CameraKeyFrameHandler {
-    private readonly keyFrames: any[] = [
+    private readonly keyFrames: CameraKeyFrame[] = [
         {
             id: 1,
             position: new Vector3(0, 60, 140),
             target: new Vector3(0, 0, 0),
             triggerType: 'keyPress',
-            triggerValue: 'Escape'
+            triggerValue: 'Escape',
         },
         {
             id: 2,
             position: new Vector3(0, 0, 0),
             target: new Vector3(24, 8, 0),
             triggerType: 'objectClick',
-            triggerValue: 'Cube037_1'
+            triggerValue: 'Cube037_1',
         }
     ]
 
@@ -44,14 +52,14 @@ export default class CameraKeyFrameHandler {
         }
     }
 
-    private addObjectClickListener(frame: any): void {
+    private addObjectClickListener(frame: CameraKeyFrame): void {
         const objectName = frame.triggerValue
         Global.objectInteractivityHandler.addClickListener(objectName, () => {
             this.update(frame)
         })
     }
 
-    private addKeyPressListener(frame: any): void {
+    private addKeyPressListener(frame: CameraKeyFrame): void {
         const key = frame.triggerValue
         window.addEventListener('keydown', (e) => {
             if (e.key === key) {
@@ -60,7 +68,7 @@ export default class CameraKeyFrameHandler {
         })
     }
 
-    private update(frame: any): void {
+    private update(frame: CameraKeyFrame): void {
         if (this.currentFrame.id !== frame.id) {
             this.currentFrame = frame
             this.camera.moveTo(frame.position)
